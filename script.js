9206b97e957aa70ec5d92265b80b53beb100c5f2
wonhee1234 <59944189+wonhee1234@users.noreply.github.com>
2020-04-22 13:57:35 -1000

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function initMap() {
+  var object1 = document.getElementById('gmaps');
+  var location1 = new google.maps.LatLng(21.283952, -157.709427);
+  var options1 = {
+    center: location1,
+    zoom: 10,
+    mapTypeId: google.maps.MapTypeId.SATELLITE,
+    mapTypeControlOptions: {
+      position: google.maps.ControlPosition.BOTTOM_CENTER
+    }
+  };
+  var gmap = new google.maps.Map(object1, options1);
+}
+google.maps.event.addDomListener(window, 'load', initMap);
+
