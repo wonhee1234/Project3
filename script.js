c0b8cd2b71f7f43706574acdca9681ef55154ec7
wonhee1234 <59944189+wonhee1234@users.noreply.github.com>
2020-04-27 16:56:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-function initMap() {
-  var object1 = document.getElementById('gmaps');
-  var location1 = new google.maps.LatLng(21.283952, -157.709427);
-  var options1 = {
-    center: location1,
-    zoom: 9,
-    mapTypeId: google.maps.MapTypeId.SATELLITE,
-    mapTypeControlOptions: {
-      position: google.maps.ControlPosition.BOTTOM_CENTER
-    }
-  };
-  var gmap = new google.maps.Map(object1, options1);
-}
-google.maps.event.addDomListener(window, 'load', initMap);
+var map;
+function initMap()
+{
+var latlang={lat:4.710859,lng:-74.074955};
+mapa=new google.maps.Map(
+		document.getElementById('gmaps'),
+		{center: new google.maps.LatLng(41.835668, -87.624221), zoom:15});
 
+var marker= new google.maps.Marker({position:latlang,map:map});
+
+marker= new google.maps.Marker({
+	map:map,
+	draggable:true,
+	animation:google.maps.Animation.BOUNCE,
+	position:new google.maps.LatLng(6.219131, -75.566329),
+//icon:'images/house6.jpg'
+});
+}
