adc9016d9be0d027f9a7e4da2cbfd270e38be073
wonhee1234 <59944189+wonhee1234@users.noreply.github.com>
2020-04-28 22:04:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 var map;
 function initMap()
 {
-var ssv={lat:41.833912, lng:-87.625547};
+var SSV={lat:41.833912, lng:-87.625547};
 var MSV={lat:41.835534, lng:-87.624167};
 var Classroom={lat:41.837409, lng:-87.626622};
 var latlang={lat:4.710859,lng:-74.074955};
-mapa=new google.maps.Map(
+map=new google.maps.Map(
 		document.getElementById('gmaps'),
 		{center: new google.maps.LatLng(41.835668, -87.624221), zoom:16});
 
-var marker= new google.maps.Marker({position:latlang,map:map});
+var MSVmarker= new google.maps.Marker({position:MSV,map:map});
+var SSVmarker= new google.maps.Marker({position:SSV,map:map});
+var Classroom= new google.maps.Marker({position:Classroom,map:map});
 
 marker= new google.maps.Marker({
 	map:map,
 	draggable:true,
 	animation:google.maps.Animation.BOUNCE,
-	position:new google.maps.LatLng(6.219131, -75.566329),
+	position:new google.maps.LatLng(41.833912,-87.625547),
 });
 }
