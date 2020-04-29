c98878c5af8eef816b9d678eccf0876a107b926e
wonhee1234 <59944189+wonhee1234@users.noreply.github.com>
2020-04-28 21:53:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var map;
 function initMap()
 {
+var ssv={lat:41.833912, lng:-87.625547};
+var MSV={lat:41.835534, lng:-87.624167};
+var Classroom={lat:41.837409, lng:-87.626622};
 var latlang={lat:4.710859,lng:-74.074955};
 mapa=new google.maps.Map(
 		document.getElementById('gmaps'),
